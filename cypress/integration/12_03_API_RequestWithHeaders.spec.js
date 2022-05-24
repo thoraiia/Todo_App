11e12e30fa9c53a38b4bc456c8fe3a754742adbf
thoraiia <thoraiia.khaled@gmail.com>
2022-05-24 22:24:59 +0200

    API Request With Headers

---
+++ cypress/integration/12_03_API_RequestWithHeaders.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress"/>
+
+describe('login API',() => {
+
+    let token;
+
+    beforeEach(() => {
+
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:8080/api/v1/users/login',
+            body:{
+                email:"hakuna.matata@gmail.com",
+                password:"Test123!"
+            }
+        }).then((response)=> {
+            token = response.body.access_token;
+        })
+    })
+
+    it('Get All Tasks', () => {
+        cy.request({
+            url: 'http://localhost:8080/api/v1/tasks',
+            headers: {
+                Authorization:`Bearer ${token}`
+            }
+        })
+    })
+
+})
